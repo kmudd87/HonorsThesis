dcb63fc86d05de1df8844ada8e98e4ea15a09f2c
kmudd87 <97718998+kmudd87@users.noreply.github.com>
2022-01-18 13:07:31 -0800

    added equivalent Hs graph to output

--- windWaveCorr.m
+++ windWaveCorr.m
@@ -93,53 +93,53 @@ function [ ] = windWaveCorr(freqTable, startTime, endTime, wgt, wgWind10m)
     ylabel('covariance')
     xlabel('frequency (s)')
     title('Wind-Wave Energy Correlation')
-%     %plot wind speeds vs equivalent Hs for each frequency band
-%     figure(3)
-%     subplot(3,3,1)
-%     scatter(wgWind10m,Hs(:,1),'r')
-%     title('22+s')
-%     subplot(3,3,2)
-%     scatter(wgWind10m,Hs(:,2),'r')
-%     title('18-22s')
-%     subplot(3,3,3)
-%     scatter(wgWind10m,Hs(:,3),'r')
-%     title('16-18s')
-%     subplot(3,3,4)
-%     scatter(wgWind10m,Hs(:,4),'r')
-%     title('14-16s')
-%     subplot(3,3,5)
-%     scatter(wgWind10m,Hs(:,5),'r')
-%     title('12-14s')
-%     subplot(3,3,6)
-%     scatter(wgWind10m,Hs(:,6),'r')
-%     title('10-12s')
-%     subplot(3,3,7)
-%     scatter(wgWind10m,Hs(:,7),'r')
-%     title('8-10s')
-%     subplot(3,3,8)
-%     scatter(wgWind10m,Hs(:,8),'r')
-%     title('6-8s')
-%     subplot(3,3,9)
-%     scatter(wgWind10m,Hs(:,9),'r')
-%     title('2-6s')
-%     sgtitle('Wind Speed vs. Wave Energy')
-%     han=axes(figure(3),'visible','off'); 
-%     han.Title.Visible='on';
-%     han.XLabel.Visible='on';
-%     han.YLabel.Visible='on';
-%     ylabel(han, 'equivalent Hs (cm)')
-%     xlabel(han, 'wind speed')
-%     %equivalent Hs - wind correlation
-%     covariance_Hs = cov([wgWind10m Hs]);
-%     ww_cov_Hs = NaN(9,1);
-%     for i = 1:9
-%         ww_cov_Hs(i) = covariance_Hs(i+1,i+1);
-%     end
-%     freqs = [22 20 17 15 13 11 9 7 4];
-%     %plot correlation term for each average frequency band value
-%     figure(4)
-%     scatter(freqs,ww_cov_Hs,'o','filled','r')
-%     ylabel('covariance')
-%     xlabel('frequency (s)')
-%     title('Wind-Wave Correlation')
+    %plot wind speeds vs equivalent Hs for each frequency band
+    figure(3)
+    subplot(3,3,1)
+    scatter(wgWind10m,Hs(:,1),'r')
+    title('22+s')
+    subplot(3,3,2)
+    scatter(wgWind10m,Hs(:,2),'r')
+    title('18-22s')
+    subplot(3,3,3)
+    scatter(wgWind10m,Hs(:,3),'r')
+    title('16-18s')
+    subplot(3,3,4)
+    scatter(wgWind10m,Hs(:,4),'r')
+    title('14-16s')
+    subplot(3,3,5)
+    scatter(wgWind10m,Hs(:,5),'r')
+    title('12-14s')
+    subplot(3,3,6)
+    scatter(wgWind10m,Hs(:,6),'r')
+    title('10-12s')
+    subplot(3,3,7)
+    scatter(wgWind10m,Hs(:,7),'r')
+    title('8-10s')
+    subplot(3,3,8)
+    scatter(wgWind10m,Hs(:,8),'r')
+    title('6-8s')
+    subplot(3,3,9)
+    scatter(wgWind10m,Hs(:,9),'r')
+    title('2-6s')
+    sgtitle('Wind Speed vs. Equivalent Hs, 9-Band')
+    han=axes(figure(3),'visible','off'); 
+    han.Title.Visible='on';
+    han.XLabel.Visible='on';
+    han.YLabel.Visible='on';
+    ylabel(han, 'equivalent Hs (cm)')
+    xlabel(han, 'wind speed')
+    %equivalent Hs - wind correlation
+    covariance_Hs = cov([wgWind10m Hs]);
+    ww_cov_Hs = NaN(9,1);
+    for i = 1:9
+        ww_cov_Hs(i) = covariance_Hs(i+1,i+1);
+    end
+    freqs = [22 20 17 15 13 11 9 7 4];
+    %plot correlation term for each average frequency band value
+    figure(4)
+    scatter(freqs,ww_cov_Hs,'o','filled','r')
+    ylabel('covariance')
+    xlabel('frequency (s)')
+    title('Wind-Equivalent Hs Correlation')
 end
